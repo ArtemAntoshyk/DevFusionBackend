ac245d96fc370a10cb8e3f2d059d9564d344c87e
artem <yaroslav.gorbatyuk.2010@gmail.com>
2024-10-23 17:15:19 +0300

    Initial commit

---
+++ src/main/java/devtitans/antoshcuk/devfusionbackend/DevFusionBackendApplication.java
@@ -0,0 +1,13 @@
+package devtitans.antoshcuk.devfusionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFusionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevFusionBackendApplication.class, args);
+    }
+
+}
